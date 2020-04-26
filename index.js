0e270e637f6e728dd8a93a8c2b64ff48c131cfa0
Moses A. Samuel <mosamuel98@gmail.com>
2020-04-26 16:57:47 -0700

    only show the game board once the player(s) have entered their name(s)

--- index.js
+++ index.js
@@ -1,4 +1,20 @@
 $(document).ready(() => {
 	const game1 = new Game.GameSession();
-	game1.startGame();
+
+	$("button[type='submit']").on("click", function (event) {
+		event.preventDefault();
+		Game.player1.name =
+			$("#p1Name")[0].value === "" ? "Player 1" : $("#p1Name")[0].value;
+		Game.player2.name =
+			$("#p2Name")[0].value === "" ? "Player 2" : $("#p2Name")[0].value;
+
+		game1.startGame();
+	});
+
+	const $allGames = $(".board");
+
+	$allGames.on("click", function (event) {
+		// console.log(event.target);
+		const targetSlot = $(this).find(event.target);
+	});
 });
