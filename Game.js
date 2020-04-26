2906cfbd2ee1890f085f1be48a8e3213f129a9a0
Moses A. Samuel <mosamuel98@gmail.com>
2020-04-26 14:05:29 -0700

    create a "Game" module with a "createBoard" method

---
+++ Game.js
@@ -0,0 +1,41 @@
+const Game = (function () {
+	return {
+		createBoard() {
+			const board = document.createElement("div");
+			$(board).addClass("board");
+
+			const row1 = Utils.createElements(["div", "div", "div"]).map(
+				(div, index) => {
+					div.classList.add("slot");
+					div.dataset.slotId = index + 1;
+					return div;
+				}
+			);
+
+			const row2 = Utils.createElements(["div", "div", "div"]).map(
+				(div, index) => {
+					div.classList.add("slot");
+					div.dataset.slotId = index + 4;
+					return div;
+				}
+			);
+
+			const row3 = Utils.createElements(["div", "div", "div"]).map(
+				(div, index) => {
+					div.classList.add("slot");
+					div.dataset.slotId = index + 7;
+					return div;
+				}
+			);
+
+			[row1, row2, row3].forEach(row => {
+				const rowContainer = document.createElement("div");
+				$(rowContainer).addClass("row");
+				Utils.renderElement({ parent: board, child: rowContainer });
+				Utils.renderElements({ parent: rowContainer, children: row });
+			});
+
+			return board;
+		},
+	};
+})();
