38db9da32c9b3500b055749702ba46797f13c609
Moses A. Samuel <mosamuel98@gmail.com>
2020-04-26 19:39:37 -0700

    render "X"s and "O"s to the screen when you click on the board

--- index.js
+++ index.js
@@ -1,20 +1,27 @@
 $(document).ready(() => {
 	const game1 = new Game.GameSession();
 
-	$("button[type='submit']").on("click", function (event) {
-		event.preventDefault();
+	$("button[type='submit']").on("click", function startGameAndAddListeners(
+		event
+	) {
+		event.preventDefault(); // prevent the page from reloading
 		Game.player1.name =
 			$("#p1Name")[0].value === "" ? "Player 1" : $("#p1Name")[0].value;
 		Game.player2.name =
 			$("#p2Name")[0].value === "" ? "Player 2" : $("#p2Name")[0].value;
 
+		// render the game board to the screen
 		game1.startGame();
-	});
 
-	const $allGames = $(".board");
+		$(document).on("gameOver", function alertWinner(event) {
+			alert(`Winner: ${event.detail.winner.name}`);
+		});
 
-	$allGames.on("click", function (event) {
-		// console.log(event.target);
-		const targetSlot = $(this).find(event.target);
+		const $allGames = $(".board");
+
+		$allGames.on("click", function (event) {
+			const targetSlot = $(this).find(event.target)[0].dataset.slotId;
+			game1.makeMove(targetSlot);
+		});
 	});
 });
