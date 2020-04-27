1e68ca4e8c5a9e1ffdf5bf5a68cabcda7569f012
Moses A. Samuel <mosamuel98@gmail.com>
2020-04-26 22:40:54 -0700

    add an "isEqual" method to the Utils module

--- Utils.js
+++ Utils.js
@@ -33,5 +33,41 @@ const Utils = (function () {
 		refresh() {
 			window.location.reload();
 		},
+
+		/**
+		 * Determines if two objects are equal by value.
+		 * It also works for arrays.
+		 * @param {(Object|Array)} o1
+		 * @param {Object|Array} o2
+		 * @returns {Boolean}
+		 */
+		isEqual(o1, o2) {
+			if (Array.isArray(o1) && Array.isArray(o2)) {
+				for (let i = 0; i < o1.length; i++) {
+					if (o1[i] === o2[i]) {
+						continue;
+					} else {
+						return false;
+					}
+				}
+				return true;
+			} else if (typeof o1 == "object" && typeof o2 == "object") {
+				const o1Keys = Object.keys(o1);
+				const o2Keys = Object.keys(o2);
+
+				for (let i = 0; i < o1Keys.length; i++) {
+					if (o1Keys[i] == o2Keys[i]) {
+						continue;
+					} else {
+						return false;
+					}
+				}
+				return true;
+			} else {
+				throw new TypeError(
+					"You must pass in either an object or an array to this function."
+				);
+			}
+		},
 	};
 })();
